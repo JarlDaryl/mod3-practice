5afc16659bac9b9518f09884d78cd07399824f6e
Luis Arques <luisarquescalero@gmail.com>
2023-10-24 11:22:10 +0200

    Ejercicio 2 RESUELTO

--- src/js/exercises/exercise-2.js
+++ src/js/exercises/exercise-2.js
@@ -3,3 +3,22 @@ EJERCICIO 2:
 Muestra el siguiente mensaje dependiendo de si un número es par o impar:
 “Number 34 is even” ó “Number 13 is odd”
 */
+
+const num = 9;
+
+
+function evenOrOdd(num) {
+if (num % 2 === 0) {
+    return 'even';
+}   else {
+    return 'odd';
+}
+}
+
+
+
+let result = `Number ${num} is ${evenOrOdd()}`;
+
+
+
+showContent(2, result);
