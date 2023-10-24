734b72647e8988ea1cd164b8c2bca0e648fcc5a0
Luis Arques <luisarquescalero@gmail.com>
2023-10-24 12:13:55 +0200

    Ejercicio 3 RESUELTO

--- src/js/exercises/exercise-3.js
+++ src/js/exercises/exercise-3.js
@@ -3,3 +3,24 @@ EJERCICIO 3:
 Muestra el siguiente mensaje con el resultado de contar el número de vocales que tiene un string:
 “Number of vowels is <result>”
 */
+
+
+let myStr = "Pancha plancha con cuatro planchas. ¿Con cuántas planchas plancha Pancha?"
+const vowels = "aeiouAEIOUáÁéÉíÍóÓúÚ"
+
+let vowelCount = 0;
+
+for (let i = 0; i < myStr.length; i++) {
+    const posAct = myStr[i];
+
+    if (vowels.includes(posAct)) {
+        vowelCount++;
+    }
+}
+
+
+let result = `The number of vowels is ${vowelCount}`;
+
+
+
+showContent(3, result);
