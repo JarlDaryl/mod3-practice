88abbe5a4df41740b5ff7ef20b20bc0ac6c4dc38
Luis Arques <luisarquescalero@gmail.com>
2023-10-24 13:00:14 +0200

    Ejercicio 4 RESUELTO

--- src/js/exercises/exercise-4.js
+++ src/js/exercises/exercise-4.js
@@ -3,3 +3,24 @@ EJERCICIO 4:
 Muestra el resultado de quitar las vocales a un string que pasamos como parámetro:
 “H, m lrnng Jvscrpt” // string original: “Hi, I am learning Javascript”
 */
+
+
+let myStr ="Hi, I am learning Javascript";
+const vowels = "aeiouAEIOUáÁéÉíÍóÓúÚ";
+
+
+let newStr = "";
+
+for (i = 0; i < myStr.length; i++) {
+    const posAct = myStr[i];
+
+    if (!vowels.includes(posAct)) {
+        newStr += posAct;
+    }
+}
+
+
+
+
+
+showContent(4, newStr);
