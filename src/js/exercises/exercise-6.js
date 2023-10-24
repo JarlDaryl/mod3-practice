f910468cfd99a3f79aedd4fd7e776fea7011bd4b
Luis Arques <luisarquescalero@gmail.com>
2023-10-24 17:26:06 +0200

    Ejercicio 6 RESUELTO

--- src/js/exercises/exercise-6.js
+++ src/js/exercises/exercise-6.js
@@ -3,3 +3,16 @@ EJERCICIO 6:
 Genera un array de 10 números aleatorios entre 0 y 100, calcula el menor y
 el mayor y muestra el resultado por pantalla
 */
+
+
+const randomNumbers = [];
+
+for (let i = 0; i < 10; i++) {
+    const randomNumb = Math.round(Math.random() * 100);
+
+    randomNumbers.push(randomNumb);
+}
+
+
+
+showContent(6, randomNumbers);
