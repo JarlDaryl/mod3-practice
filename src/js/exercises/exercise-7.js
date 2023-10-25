e37fbac4dda0754828607314a5557b326f08ceb8
Luis Arques <luisarquescalero@gmail.com>
2023-10-25 12:06:18 +0200

    Ejercicio 7 RESUELTO

--- src/js/exercises/exercise-7.js
+++ src/js/exercises/exercise-7.js
@@ -4,10 +4,17 @@ Escribir una función que calcule si una cadena de texto es un palíndromo y
 devuelva un mensaje con la cadena de texto y el resultado
 */
 
+const text = "Logra Casillas alli sacar gol";
 
+function esPalindromo(text) {
+    const nuevaCadena = text.toLowerCase().replaceAll(" ", "");
+    const cadenaInvertida = nuevaCadena.split('').reverse().join('');
 
+    if (nuevaCadena === cadenaInvertida) {
+        return `La cadena "${text}" es un palíndromo.`;
+    } else {
+        return `La cadena "${text}" no es un palíndromo.`;
+    }
+}
 
-
-
-
-showContent(6, randomNumbers);
+showContent(7, esPalindromo(text));
