39be76374808d854972749852f003615b6b66289
Luis Arques <luisarquescalero@gmail.com>
2023-10-25 18:29:12 +0200

    Ejercicio 11 RESUELTO

--- src/js/exercises/exercise-11.js
+++ src/js/exercises/exercise-11.js
@@ -2,3 +2,11 @@
 EJERCICIO 11:
 Ordena las peliculas por valoracion (propiedad "rating") de mayor a menor
 */
+
+
+function sortRating(a, b) {
+    return b.rating - a.rating;
+}
+movies.sort(sortRating);
+
+showContent(11, movies);
