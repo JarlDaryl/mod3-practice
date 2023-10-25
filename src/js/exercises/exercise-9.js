62364510544ff483bc619f089846d80034f5d7b1
Luis Arques <luisarquescalero@gmail.com>
2023-10-25 16:26:53 +0200

    Ejercicio 9 RESUELTO

--- src/js/exercises/exercise-9.js
+++ src/js/exercises/exercise-9.js
@@ -3,3 +3,23 @@ EJERCICIO 9:
 Genera un array de 10 números aleatorios entre 0 y 100, calcula el menor y
 el mayor y muestra el resultado por pantalla
 */
+
+
+
+const randomNumberS = [];
+
+for (let i = 0; i < 10; i++) {
+    const randomNumB = Math.round(Math.random() * 100);
+
+    randomNumberS.push(randomNumB);
+
+    
+}
+
+const minNumbeR = Math.min(...randomNumberS);
+const maxNumbeR = Math.max(...randomNumberS);
+
+
+let result2= "Mínimo: " + minNumbeR + " Máximo: " + maxNumbeR+ " Array: " + randomNumberS;
+
+showContent(9, result2);
